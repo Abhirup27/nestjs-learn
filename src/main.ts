5f10c0fabf66650fa72d8e7418ed5ef49baf9320
Abhirup Bhattacharyya <55529237+Abhirup27@users.noreply.github.com>
2024-12-01 23:51:51 +0530

    Decorators

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,3 @@ async function bootstrap() {
 }
 bootstrap();
 
-const nums = [1, 2, 3, 4];
-const doubled = nums.map(n => n * 2);
-console.log(doubled);
-
-function mult(factor:number):(num: number) => number
-{
-  return (num: number): number => num * factor;
-}
-const triple: (num: number) => number = mult(3);
-
-const result: number = triple(5);
-console.log(result);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,28 @@
-import { Controller, Get, Post, Delete, Put, Patch } from "@nestjs/common"
+import { Controller, Get, Post, Delete, Put, Patch, Param, Query, Body, Req, Headers, Ip } from "@nestjs/common"
 
-
+    type p={
+        id: number
+    };
 @Controller('users')
 export class UsersController { 
-    @Get()
-    public getUsers() : string
+
+
+    @Get('/:id/:option?')
+    public getUsers(@Param() params: any, @Query() query: any) : string
     {
+        console.log(params.id);
+        console.log(query);
         return 'You Sent a get request to /users endpoint';
     }
 
     @Post()
-    public createUser(): string
+    public createUser(@Body() request :Request, @Headers() headers: any, @Ip() ip: any): string
     {
+
+        console.dir(typeof request);
+        console.log(request)
+        console.log(headers);
+        console.log(ip);
         return 'You sent a POST request to /users endpoint to create an user.';
     }
 }
