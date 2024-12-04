d7465659ba32eaa2c8aedb7f28fbc0e7823db898
Abhirup Bhattacharyya <55529237+Abhirup27@users.noreply.github.com>
2024-12-04 09:44:47 +0530

    learned how to add global pipes, whitelisting, setting DTOs to params, transform the validated request body to the instance/ type of the DTO type

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Delete, Put, Patch, Param, Query, Body, Req, Headers, Ip, ParseIntPipe,DefaultValuePipe, ValidationPipe } from "@nestjs/common"
 import { CreateUserDto } from "./dtos/create-user.dto";
+import { GetUsersParamDto } from "./dtos/get-users-params.dto";
 
     // type p={
     //     id: number
@@ -8,28 +9,28 @@ import { CreateUserDto } from "./dtos/create-user.dto";
 export class UsersController { 
 
 
-    @Get('/:id?/:option?')
-    public getUsers(@Param('id', ParseIntPipe) id: number | undefined,
+    @Get('/:id?') //'/:id?/:option?'
+    public getUsers(@Param() getUserParamDto: GetUsersParamDto, //@Param('id', ParseIntPipe) id: number | undefined
         @Query('limit',new DefaultValuePipe(10), ParseIntPipe) limit: number,
         @Query('page',new DefaultValuePipe(1), ParseIntPipe) page : number
     ): string
     {
-        console.log(id);
-        console.log(typeof id);
+        console.log(getUserParamDto);
+        console.log(typeof getUserParamDto);
         console.log(limit);console.log(page);
         return 'Sent a get request to /users endpoint';
     }
 
     @Post()
     public createUser(
-        @Body(new ValidationPipe()) createUserDto: CreateUserDto,
+        @Body(new ValidationPipe({whitelist: true ,forbidNonWhitelisted:true, transform: true})) createUserDto: CreateUserDto, // we can remove new ValidationPipe from here because we added a global ValidationPipe in main.ts
         @Headers() headers: any,
         @Ip() ip: any): string
     {
 
         //console.dir(typeof request);
         // console.log(request)
-        console.log(createUserDto);
+        console.log(createUserDto instanceof CreateUserDto);
         console.log(headers);
         console.log(ip);
         return 'sent a POST request to /users endpoint to create an user.';
