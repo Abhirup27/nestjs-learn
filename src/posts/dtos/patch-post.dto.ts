8723ddcf5156ea57b2a1f4d5082cb1e511251ca6
Abhirup Bhattacharyya <55529237+Abhirup27@users.noreply.github.com>
2024-12-23 23:33:40 +0530

    mapped-types

---
+++ src/posts/dtos/patch-post.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { PartialType } from "@nestjs/mapped-types";
+import { CreatePostDto } from "./create-post.dto";
+import { IsNotEmpty, IsInt } from "class-validator";
+
+
+export class PatchPostDto extends PartialType(CreatePostDto){
+
+    @ApiProperty(
+        {
+            description: "The ID of the post that needs to be updated"
+        }
+    )
+    @IsInt()
+    @IsNotEmpty()
+    id: number;
+}
